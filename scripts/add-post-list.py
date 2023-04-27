8e641f673605b0669153f0e5285f675ca9d3362b
yym68686 <yym68686@outlook.com>
2023-04-27 08:45:13 +0800

    Thu Apr 27 08:45:13 CST 2023

--- scripts/add-post-list.py
+++ scripts/add-post-list.py
@@ -22,18 +22,50 @@ for subdir in all_files:
             md_files_path += ["./post/" + f"{subdir}/index.md"]
             print("=>", "./post/" + f"{subdir}/index.md")
 
-with open("index.md", "r") as f:
-    md_content = f.read()
-title = "# 文章列表"
-index = md_content.find(title)
-new_content = ""
-for mdpath in md_files_path:
-    with open(mdpath, "r") as f:
-        post_content = f.read()
-    title = gettitle(post_content)
-    new_content += f"- [{title}]({mdpath})\n"
-md_content = md_content[:index + len(title) - 4] + new_content + "\n" + md_content[index + len(title) - 4:]
-with open("index.md", "w") as f:
-    f.write(md_content)
+# coding=utf8
+# 上述标签定义了本文档的编码，与Python 2.x兼容。
+
+import re
+
+regex = r"##\s文章列表([.\n]*(-\s.*\n)*\n*)##"
+
+test_str = ("# yym68686\n\n"
+	"## 联系方式\n\n"
+	"✈️ [Telegram](https://t.me/yym68686)\n\n"
+	"🐦 [Twitter](https://twitter.com/yym68686)\n\n"
+	"📖 [GitHub](https://github.com/yym68686)\n\n"
+	"📮 Email: yym68686@outlook.com\n\n"
+	"## 文章列表\n\n"
+	"- [一个极简主义者的博客页面](./post/develop-purepage/index.md)\n"
+	"- [Re-ID 综述论文分享](./post/reid-outlook-paper-share/index.md)\n\n"
+	"## 关于我\n\n"
+	"code is law.")
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    print ("在{start}-{end}找到匹配{matchNum}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        
+        print ("在{start}-{end}找到组{groupNum}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
+# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+
+# with open("index.md", "r") as f:
+#     md_content = f.read()
+# title = "# 文章列表"
+# index = md_content.find(title)
+# new_content = ""
+# for mdpath in md_files_path:
+#     with open(mdpath, "r") as f:
+#         post_content = f.read()
+#     title = gettitle(post_content)
+#     new_content += f"- [{title}]({mdpath})\n"
+# md_content = md_content[:index + len(title) - 4] + new_content + "\n" + md_content[index + len(title) - 4:]
+# with open("index.md", "w") as f:
+#     f.write(md_content)
 
 os.system(f'cd {path} && git add . && git commit -m "$(date)" && git push origin $(git name-rev --name-only HEAD)')
